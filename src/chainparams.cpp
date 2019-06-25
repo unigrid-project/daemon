d7377338fb95feb8022923911aa54a2da4c730e6
Adam Waldenberg <adam@unigrid.org>
2019-06-25 04:38:41 +0200

    Fix genesis block timestamp string

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -161,7 +161,7 @@ public:
          *     CTxOut(nValue=50.00000000, scriptPubKey=0xA9037BAC7050C479B121CF)
          *   vMerkleTree: e0028e
          */
-        const char* pszTimestamp = "UNIGRID Genesis Block: 2018/09/05 12:00:00";
+        const char* pszTimestamp = "HUZU Genesis Block: 2018/09/05 12:00:00";
         CMutableTransaction txNew;
         txNew.vin.resize(1);
         txNew.vout.resize(1);
