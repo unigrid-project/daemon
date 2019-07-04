cf1cc1a5e1aca858aa7e826eb96e29aa3cb4e1eb
Adam Waldenberg <adam@unigrid.org>
2019-07-04 16:29:14 +0200

    Switch to the QT 5.9.x recommended freetype version 2.6.1

--- depends/packages/freetype.mk
+++ depends/packages/freetype.mk
@@ -1,11 +1,11 @@
 package=freetype
-$(package)_version=2.7.1
+$(package)_version=2.6.1
 $(package)_download_path=http://download.savannah.gnu.org/releases/$(package)
 $(package)_file_name=$(package)-$($(package)_version).tar.bz2
-$(package)_sha256_hash=3a3bb2c4e15ffb433f2032f50a5b5a92558206822e22bfe8cbe339af4aa82f88
+$(package)_sha256_hash=2f6e9a7de3ae8e85bdd2fe237e27d868d3ba7a27495e65906455c27722dd1a17
 
 define $(package)_set_vars
-  $(package)_config_opts=--without-zlib --without-png --disable-static
+  $(package)_config_opts=--without-zlib --without-png --without-harfbuzz --without-bzip2 --disable-static
   $(package)_config_opts_linux=--with-pic
 endef
 
