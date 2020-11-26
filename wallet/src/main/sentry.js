989c568be9246728cbce478980c4c45639e16f06
Adam Waldenberg <adam@unigrid.org>
2020-11-26 13:24:37 +0100

    Remove wallet directory
    These files will be placed in the janus repo from now on.

--- wallet/src/main/sentry.js
+++
@@ -1,7 +0,0 @@
-import * as Sentry from "@sentry/electron";
-
-Sentry.init({
-	dsn: "https://51f7c29baa594b0a8bfa788113e696ce@o266736.ingest.sentry.io/5427751",
-    release: 'unigrid-electron@' + process.env.npm_package_version,
-    debug: true
-});
